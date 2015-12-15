399973d4dc5445f3bf8baa4f5b940d053bfc0cb2
Ryan Glover <me@ryanglover.net>
2015-12-15 11:13:57 -0600

    Initial commit.

---
+++ code/both/routes/authenticated.js
@@ -0,0 +1,17 @@
+const authenticatedRoutes = FlowRouter.group({
+  name: 'authenticated'
+});
+
+authenticatedRoutes.route( '/', {
+  name: 'index',
+  action() {
+    BlazeLayout.render( 'default', { yield: 'index' } );
+  }
+});
+
+authenticatedRoutes.route( '/dashboard', {
+  name: 'dashboard',
+  action() {
+    BlazeLayout.render( 'default', { yield: 'dashboard' } );
+  }
+});
